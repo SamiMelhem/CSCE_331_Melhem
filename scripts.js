f4cfdd22ddfb9c52db02bbf48811c7f3c8d41737
Sami Melhem <SamiISaboss23@gmail.com>
2024-09-12 22:16:36 -0500

    Added a home button on pages other than the home page

--- scripts.js
+++ scripts.js
@@ -33,28 +33,19 @@ showSlides(slideIndex);
 // Auto slide functionality
 let slideInterval = setInterval(() => { plusSlides(1); }, 2000); 
 
-function switchStyle() {
-  var toggle = document.getElementById("style-toggle");
-  if (toggle.checked) {
-      document.getElementById("style-link").setAttribute("href", "styles2.css");
-      localStorage.setItem("preferredStyle", "styles2.css");
-  } else {
-      document.getElementById("style-link").setAttribute("href", "styles.css");
-      localStorage.setItem("preferredStyle", "styles.css");
-  }
+function toggleStyleSheet(){
+  var styleSheet = document.getElementById("style-link"); // mainStyleSheet
+  var currentStyle = styleSheet.getAttribute("href");
+  var newStyle = currentStyle === "styles.css" ? "styles2.css" : "styles.css";
+  styleSheet.setAttribute("href", newStyle);
+
+  localStorage.setItem("currentStyle", newStyle);
 }
 
-function loadPreferredStyle() {
-  var savedStyle = localStorage.getItem("preferredStyle");
-  if (savedStyle === "styles2.css") {
-      document.getElementById("style-link").setAttribute("href", "styles2.css");
-      document.getElementById("style-toggle").checked = true;
-  } else {
-      document.getElementById("style-link").setAttribute("href", "styles.css");
-      document.getElementById("style-toggle").checked = false;
+window.onload = function(){
+  var savedStyle = localStorage.getItem("currentStyle");
+  var styleSheet = document.getElementById("mainStyleSheet");
+  if (savedStyle) {
+      styleSheet.setAttribute("href", savedStyle);
   }
 }
-
-window.onload = function() {
-  loadPreferredStyle();
-};
